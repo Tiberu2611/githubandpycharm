0d5237df2b7997914b76ed26176d200fa01e8038
Tiberu2611 <91483220+Tiberu2611@users.noreply.github.com>
2021-11-09 21:25:13 +0700

    Update BT_11092021.py

--- BT_11092021.py
+++ BT_11092021.py
@@ -1,3 +1,7 @@
+# Bài 1: Viết chương trình thực hiện các yêu cầu sau:
+# - Nhập một chuỗi kí tự từ bàn phím
+# - Nhập tên tập tin từ bàn phím
+# - Lưu chuỗi ký tự ở trên vào tập tin.
 file_object = open('BT1', mode= 'w')
 file_object.write('Chao moi nguoi')
 file_object.close()
